d39480b3b25da556f1e0fd2d2bbe553041016e46
Jerson La Torre <jerson.latorre@gmail.com>
2024-04-07 02:23:32 -0500

    re-estructuración y renombrados

--- scripts/export.sh
+++ scripts/export.sh
@@ -5,10 +5,10 @@ set -e
 
 # Si hay cambios, proceder con el proceso de compilación, add, commit, y push
 echo "Compilando..."
-node_modules/.bin/parcel build ./src/pose-detector.js --dist-dir ./dist
+node_modules/.bin/parcel build ./src/body-tracker.js --dist-dir ./dist
 
 # Envía el archivo compilado a /Users/uno/Desktop/blazepose-html
 echo "Enviando archivo compilado a /Users/uno/Desktop/blazepose-html-3..."
-cp ./dist/pose-detector.js /Users/uno/Desktop/blazepose-html-3/pose-detector.js
+cp ./dist/body-tracker.js /Users/uno/Desktop/blazepose-html-3/body-tracker.js
 
 echo "Listo!"
